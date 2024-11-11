cb2a2c6660ca1ba40204b9258aff2087157028fa
Vladimir Manzhos <manzhos@gmail.com>
2024-11-11 08:02:57 +0100

    modified:   src/component/TelegramChat.jsx 	modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 //dev
-// export const API_URL  = 'http://localhost:3300/'
-// export const URL      = 'http://localhost:3000/'
+export const API_URL  = 'http://localhost:3300/'
+export const URL      = 'http://localhost:3000/'
 
 //production
-export const API_URL  = 'https://lazzarottiassociati.com/'
-export const URL      = 'https://lazzarottiassociati.com/'
+// export const API_URL  = 'https://lazzarottiassociati.com/'
+// export const URL      = 'https://lazzarottiassociati.com/'
 
 export const telegramBotToken = '1981375402:AAFAM1J-bOVw74QpXnesylNSFbi2XTpUxQk'
-export const telegramBotId = '114111581'
+export const telegramBotId = '371598534'
